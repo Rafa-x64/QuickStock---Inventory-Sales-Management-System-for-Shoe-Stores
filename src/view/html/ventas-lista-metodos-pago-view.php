4144e1bb12dcb58b9f556d191b1513dab84a2b9d
Rafael <alvarezrafaelat@gmail.com>
2025-10-31 18:49:10 -0400

    vistas agregadas pendiente editar las vistas para adaptarlas a MER y agregar contenido a las nuevas vistas registradas

---
+++ src/view/html/ventas-lista-metodos-pago-view.php
@@ -0,0 +1,9 @@
+<div class="container-fluid" id="mainContent">
+    <div class="row">
+        <div class="col-12 p-5">
+            <div class="row d-flex flex-row justify-content-center align-items-center">
+
+            </div>
+        </div>
+    </div>
+</div>
